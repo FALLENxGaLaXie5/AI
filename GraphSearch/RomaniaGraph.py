3581b50f67af273041e20b8c7d60e242b70d922f
Steward <joshua.steward095@sie-1242-03.csuci.edu>
2017-09-26 09:46:27 -0700

    graph hw

--- GraphSearch/RomaniaGraph.py
+++ GraphSearch/RomaniaGraph.py
@@ -9,6 +9,7 @@ from GraphAI import Graph, Vertex
 
 #-----------------------------------------------------------
 v = list()
+'''''
 v.append(Vertex('Arad', 366))    
 v.append(Vertex('Bucharest', 0))    
 v.append(Vertex('Craiova', 160))    
@@ -29,38 +30,46 @@ v.append(Vertex('Timisoara', 329))
 v.append(Vertex('Urziceni', 80))    
 v.append(Vertex('Vaslui', 199))    
 v.append(Vertex('Zerind', 374))    
+'''
+v.append(Vertex('C1', 0))
+v.append(Vertex('C2', 0))
+v.append(Vertex('C3', 0))
+v.append(Vertex('C4', 0))
+v.append(Vertex('C5', 0))
+v.append(Vertex('C6', 0))
+v.append(Vertex('C7', 0))
+v.append(Vertex('C8', 0))
+v.append(Vertex('C9', 0))
+v.append(Vertex('C10', 0))
+v.append(Vertex('C11', 0))
+v.append(Vertex('C12', 0))
+v.append(Vertex('C13', 0))
+v.append(Vertex('C14', 0))
+v.append(Vertex('C15', 0))
 
 #-----------------------------------------------------------
 graph = Graph(directed=False)
 
-for i in range (0, 20):
+for i in range (0, 15):
     graph.AddVertex(v[i])
 
-graph.SetStartVertex(v[0])
-graph.SetGoalVertex(v[1])
+graph.SetStartVertex(v[2])
+graph.SetGoalVertex(v[14])
 
-graph.AddEdge(v[0], v[15], 140)
-graph.AddEdge(v[0], v[16], 118)
-graph.AddEdge(v[0], v[19], 75)
+graph.AddEdge(v[0], v[1], 1)
+graph.AddEdge(v[0], v[2], 1)
 
-graph.AddEdge(v[16], v[9], 111)
-graph.AddEdge(v[15], v[14], 80)
-graph.AddEdge(v[15], v[5], 99)
-graph.AddEdge(v[19], v[12], 71)
-
-graph.AddEdge(v[9], v[10], 70)
-
-graph.AddEdge(v[14], v[2], 146)
-graph.AddEdge(v[14], v[13], 97)
-
-graph.AddEdge(v[5], v[1], 211)
- 
-graph.AddEdge(v[12], v[15], 151)
-
-graph.AddEdge(v[10], v[3], 75)
-
-graph.AddEdge(v[3], v[2], 120)
-
-graph.AddEdge(v[2], v[13], 138)
-
-graph.AddEdge(v[13], v[1], 101)
+graph.AddEdge(v[1], v[3], 1)
+graph.AddEdge(v[3], v[4], 1)
+graph.AddEdge(v[2], v[5], 1)
+graph.AddEdge(v[5], v[8], 1)
+graph.AddEdge(v[8], v[9], 1)
+graph.AddEdge(v[4], v[7], 1)
+graph.AddEdge(v[7], v[9], 1)
+graph.AddEdge(v[9], v[10], 1)
+graph.AddEdge(v[4], v[6], 1)
+graph.AddEdge(v[6], v[13], 1)
+graph.AddEdge(v[13], v[14], 1)
+graph.AddEdge(v[9], v[11], 1)
+graph.AddEdge(v[11], v[12], 1)
+graph.AddEdge(v[12], v[14], 1)
